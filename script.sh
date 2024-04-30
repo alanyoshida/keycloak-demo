c6e063b1d30af6ebae2ad51b9a5aa16ca4616b06
Alan Yoshida <alanyoshida@msn.com>
2024-04-30 19:16:06 -0300

    metallb + dnsmasq

--- script.sh
+++ script.sh
@@ -5,19 +5,21 @@ COLOR_GREEN="32"
 COLOR_YELLOW="33"
 COLOR_BLUE="34"
 
-RED="\e[${COLOR_RED}m;"
-GREEN="\e[${COLOR_GREEN}m;"
-YELLOW=="\e[${COLOR_YELLOW}m;"
-BLUE=="\e[${COLOR_BLUE}m;"
+RED="\e[${COLOR_RED}m"
+GREEN="\e[${COLOR_GREEN}m"
+YELLOW=="\e[${COLOR_YELLOW}m"
+BLUE=="\e[${COLOR_BLUE}m"
 
 BOLD="\e[1m"
 BOLDGREEN="\e[1;${COLOR_GREEN}m"
 BOLDRED="\e[1;${COLOR_RED}m"
+BOLDBLUE="\e[1;${COLOR_BLUE}m"
 ITALICRED="\e[3;${COLOR_RED}m"
 CLEARFORMAT="\e[0m"
 
 check_dependencies(){
     check go
+    check yq
     check git
     check gum
     check kind
@@ -37,7 +39,6 @@ check(){
 
 up (){
   CONFIG=$(kubectl config current-context)
-  # echo -e "CONFIG=$CONFIG"
   if [ "$CONFIG" == "kind-kind" ]; then
     tilt up
   else
@@ -46,24 +47,40 @@ up (){
 }
 
 setup (){
+    # DEPENDENCIES
     echo -e "${BOLD}Checking project dependencies ...${CLEARFORMAT}"
+    check_dependencies
+
+    # CREATE KIND CLUSTER
     gum confirm "Quer criar um novo cluster kind ?"
     if [ $? -eq 0 ]; then
         echo -e "\n${BOLD}Creating kind cluster${CLEARFORMAT}"
         bash ./kind-with-registry.sh
+        kubectl wait -A --for=condition=ready pod --field-selector=status.phase!=Succeeded --timeout=5m
     fi
-    gum spin --show-output --title "Waiting 10s for cluster ..." -- sleep 10
-    CONFIG=$(kubectl config current-context)
-    # echo -e "CONFIG=$CONFIG"
-    if [ "$CONFIG" == "kind-kind" ]; then
-        gum confirm "Quer instalar o nginx no cluster $CONFIG ?"
-        if [ $? -eq 0 ]; then
-            echo -e "kubectl apply -f ./nginx-ingress/deploy.yaml"
-            kubectl apply -f ./nginx-ingress/deploy.yaml
-        fi
-        echo -e "\n${BOLD}Starting tilt${CLEARFORMAT}"
-        tilt up
+    # gum spin --show-output --title "Waiting 10s for cluster ..." -- sleep 10
+
+    # METAL LB
+    gum confirm "Deseja configurar o metallb?"
+    if [ $? -eq 0 ]; then
+      echo -e "\n${BOLD}Configuring metallb${CLEARFORMAT}"
+      DOCKER_CIDR=$(docker network inspect kind -f '{{(index .IPAM.Config 0).Subnet}}')
+      DOCKER_CIDR_2_OCTECTS=$(echo $DOCKER_CIDR | sed -E 's/([0-9]{0,3}\.[0-9]{1,3}).*/\1/')
+      cat charts/metallb/values.yaml | yq ".metallb.IPAddressPool.addresses[0]=\"$DOCKER_CIDR_2_OCTECTS.255.200-$DOCKER_CIDR_2_OCTECTS.255.250\"" > charts/metallb/values.yaml
     fi
+
+    # DNSMASQ
+    gum confirm "Quer configurar o dnsmasq?"
+    if [ $? -eq 0 ]; then
+      LB_IP=$(kubectl get svc -n ingress-nginx ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+      # point kind.cluster domain (and subdomains) to our load balancer
+      echo "address=/cluster.local/$LB_IP" | sudo tee /etc/dnsmasq.d/kind.k8s.conf
+      # restart dnsmasq
+      sudo systemctl restart dnsmasq
+    fi
+
+    # K8S CONFIG
+    up
 }
 
 check_folder() {
@@ -91,36 +108,20 @@ ${BOLD}Usage: script.sh [OPTIONS]${CLEARFORMAT}
 
 ${BOLD}Options:${CLEARFORMAT}
     up             Runs tilt up if in context kind
-    setup          Configure everything, create cluster and start tilt
-    clone          Clone repos
-    link           Create repo symlinks
+    setup, build   Configure everything, create cluster and start tilt
     dependencies   Check if you have the requirements
     -h, --help     Print Help
 "
 echo -e "$__usage"
 }
 
-print_make_help(){
-__usage="
-${BOLD}Usage: make [OPTIONS]${CLEARFORMAT}
-
-${BOLD}Options:${CLEARFORMAT}
-    up              Runs 'tilt up' if in context kind
-    setup           Configure everything, create cluster and start tilt
-    clone           Clone repos
-    link            Create repo symlinks
-    dependencies    Check if you have the requirements
-    create-cluster  Create kind cluster
-"
-echo -e "$__usage"
-}
-
 print_start(){
-__usage="
-####################################
-#       PROJECT                    #
-####################################
+__usage="${BOLDBLUE}
+=================
+# Script start  #
+=================${CLEARFORMAT}
 "
+
 echo -e "$__usage"
 }
 
@@ -134,12 +135,6 @@ case "$1" in
   setup|build)
     setup
     ;;
-  clone)
-    clone_repos
-    ;;
-  link)
-    symlinks
-    ;;
   dependencies)
     check_dependencies
     ;;
@@ -148,9 +143,6 @@ case "$1" in
     ;;
   "--help"|"-h")
     print_help
-    ;;
-  "make-help")
-    print_make_help
     ;;
   *)
     echo -e "\e[31merror: Parameter not found.\e[0m"
